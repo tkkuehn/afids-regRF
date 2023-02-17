8257c5dea7895b2026609f82b09080dbf459372e
Tristan Kuehn <tristankuehn@gmail.com>
2023-02-17 16:49:57 -0500

    Update training refactor

--- afids_regrf/train.py
+++ afids_regrf/train.py
@@ -2,8 +2,9 @@
 from __future__ import annotations
 
 import itertools as it
+from collections.abc import Iterable, Sequence
 from os import PathLike
-from typing import Optional, Sequence
+from typing import Optional
 
 import nibabel as nib
 import numpy as np
@@ -13,7 +14,6 @@ from numpy.typing import NDArray
 from sklearn.ensemble import RandomForestRegressor
 
 # Constants -- Should parameterize our functions instead I think
-PAD_FLAG = False
 PADDING = 0
 SIZE = 1
 SAMPLING_RATE = 5
@@ -89,18 +89,18 @@ def sample_coord_region(
         it.product(
             *[
                 range(
-                    coord[0] - (sampling_rate * multiplier),
-                    coord[0] + (sampling_rate * multiplier + 1),
+                    coord[0] - sampling_rate * multiplier,
+                    coord[0] + (sampling_rate * multiplier) + 1,
                     multiplier,
                 ),
                 range(
                     coord[1] - sampling_rate * multiplier,
-                    coord[1] + (sampling_rate * multiplier + 1),
+                    coord[1] + (sampling_rate * multiplier) + 1,
                     multiplier,
                 ),
                 range(
-                    coord[2] - sampling_rate,
-                    coord[2] + (sampling_rate * multiplier + 1),
+                    coord[2] - sampling_rate * multiplier,
+                    coord[2] + (sampling_rate * multiplier) + 1,
                     multiplier,
                 ),
             ]
@@ -108,18 +108,106 @@ def sample_coord_region(
     )
 
 
+def gen_offset_corners(
+    num_offsets: int = 4000,
+    lower_range: range = range(-17, 15),
+    size_range: range = range(1, 4),
+    rng: Optional[np.random.Generator] = None,
+) -> tuple[NDArray, NDArray]:
+    """Generate the lower and upper corners of n boxes relative to the origin.
+
+    "Lower" here refers to corners that are closer to the origin.
+
+    Parameters
+    ----------
+    num_offsets:
+        Number of offset corners to generate
+    lower_range
+        Distance along each axis the lower corner is allowed to be.
+    size_range
+        Range of allowed sizes of the box along each dimension.
+    """
+    rng = rng if rng else np.random.default_rng()
+    upper_corners = (
+        lower_corners := rng.choice(lower_range, size=(num_offsets, 3))
+    ) + rng.choice(size_range, size=(num_offsets, 3))
+    return lower_corners, upper_corners
+
+
+def gen_box_averages(img: NDArray, corner_list: NDArray) -> NDArray:
+    """For every box defined by corner_list, compute the average voxel value.
+
+    Parameters
+    ----------
+    img
+        An image containing all the boxes defined by corner_list.
+    corner_list
+        An n x 6 array of pairs of corners defining boxes within img.
+    """
+    # compute the integral image for more efficient generation of haar-like features
+    iv_image = integral_volume(img)
+
+    # n x 1 array of the sum of the voxel values in each box
+    # See Cui et al. Fig. 2
+    voxel_sums = (
+        iv_image[corner_list[:, 3] + 1, corner_list[:, 4] + 1, corner_list[:, 5] + 1]
+        - iv_image[corner_list[:, 0], corner_list[:, 4] + 1, corner_list[:, 5] + 1]
+        - iv_image[corner_list[:, 3] + 1, corner_list[:, 4] + 1, corner_list[:, 2]]
+        - iv_image[corner_list[:, 3] + 1, corner_list[:, 1], corner_list[:, 5] + 1]
+        - iv_image[corner_list[:, 0], corner_list[:, 1], corner_list[:, 2]]
+        + iv_image[corner_list[:, 3] + 1, corner_list[:, 1], corner_list[:, 2]]
+        + iv_image[corner_list[:, 0], corner_list[:, 1], corner_list[:, 5] + 1]
+        + iv_image[corner_list[:, 0], corner_list[:, 4] + 1, corner_list[:, 2]]
+    )
+
+    # n x 1 array of the number of voxels in each box
+    nums_box_voxel = (
+        (corner_list[:, 3] - corner_list[:, 0] + 1)
+        * (corner_list[:, 4] - corner_list[:, 1] + 1)
+        * (corner_list[:, 5] - corner_list[:, 2] + 1)
+    )
+
+    # n x 1 array of the average voxel value in each box
+    return voxel_sums / nums_box_voxel
+
+
+def gen_feature_boxes(
+    feature_offset_corners: tuple[NDArray, NDArray], all_samples: NDArray
+):
+    """Generate a set of boxes from which to compute Haar-like features.
+
+    Parameters
+    ----------
+    feature_offset_corners
+        The lower and upper corner offsets for each box
+    all_samples
+        The sample points relative to which the boxes will be defined
+    """
+    lower_offsets, higher_offsets = feature_offset_corners
+
+    # Generate 4000 bounding boxes near each feature
+    min_corner_list = np.zeros((4000 * all_samples.shape[0], 3)).astype("uint8")
+    max_corner_list = np.zeros((4000 * all_samples.shape[0], 3)).astype("uint8")
+    for idx in range(all_samples.shape[0]):
+        min_corner_list[idx * 4000 : (idx + 1) * 4000] = (
+            all_samples[idx] + lower_offsets
+        )
+        max_corner_list[idx * 4000 : (idx + 1) * 4000] = (
+            all_samples[idx] + higher_offsets
+        )
+    return np.hstack((min_corner_list, max_corner_list))
+
+
 def gen_features(
     img_path: PathLike | str,
-    fcsv_path: PathLike | str,
-    feature_offsets_path: PathLike | str,
-    fiducial_num: int,
+    fiducial: NDArray,
+    feature_offset_corners: tuple[NDArray, NDArray],
 ) -> list[NDArray]:
     """Generate features for one image and fiducial."""
     aff, img = read_nii_metadata(img_path)
 
     # Get and compute new fiducial location
-    fid_world = get_fid(fcsv_path, fiducial_num - 1)
-    resampled_fid = fid_world2voxel(fid_world, aff, resample_size=SIZE, padding=PADDING)
+    resampled_fid = fid_world2voxel(fiducial, aff, resample_size=SIZE, padding=PADDING)
 
     # Get image samples (sample more closer to target)
     # Concatenate and retain unique samples and
@@ -131,84 +219,48 @@ def gen_features(
         axis=0,
     )
 
-    # Compute Haar-like features features
-    # Make this optional to load or create
-    feature_offsets_data = np.load(feature_offsets_path)
-    smin, smax = feature_offsets_data["arr_0"], feature_offsets_data["arr_1"]
-
-    # Generate bounding cube surrounding features
-    min_corner_list = np.zeros((4000 * all_samples.shape[0], 3)).astype("uint8")
-    max_corner_list = np.zeros((4000 * all_samples.shape[0], 3)).astype("uint8")
-
-    for idx in range(all_samples.shape[0]):
-        min_corner_list[idx * 4000 : (idx + 1) * 4000] = all_samples[idx] + smin
-        max_corner_list[idx * 4000 : (idx + 1) * 4000] = all_samples[idx] + smax
-
-    corner_list = np.hstack((min_corner_list, max_corner_list))
-
-    # compute the integral image for more efficient generation of haar-like features
-    iv_image = integral_volume(img)
-
-    # intialize a numpy array to store features
-    testerarr = np.zeros((4000 * all_samples.shape[0]))
-
-    numerator = (
-        iv_image[corner_list[:, 3] + 1, corner_list[:, 4] + 1, corner_list[:, 5] + 1]
-        - iv_image[corner_list[:, 0], corner_list[:, 4] + 1, corner_list[:, 5] + 1]
-        - iv_image[corner_list[:, 3] + 1, corner_list[:, 4] + 1, corner_list[:, 2]]
-        - iv_image[corner_list[:, 3] + 1, corner_list[:, 1], corner_list[:, 5] + 1]
-        - iv_image[corner_list[:, 0], corner_list[:, 1], corner_list[:, 2]]
-        + iv_image[corner_list[:, 3] + 1, corner_list[:, 1], corner_list[:, 2]]
-        + iv_image[corner_list[:, 0], corner_list[:, 1], corner_list[:, 5] + 1]
-        + iv_image[corner_list[:, 0], corner_list[:, 4] + 1, corner_list[:, 2]]
+    box_averages = gen_box_averages(
+        img, gen_feature_boxes(feature_offset_corners, all_samples)
     )
 
-    denominator = (
-        (corner_list[:, 3] - corner_list[:, 0] + 1)
-        * (corner_list[:, 4] - corner_list[:, 1] + 1)
-        * (corner_list[:, 5] - corner_list[:, 2] + 1)
-    )
-
-    # dump features into intialized variable
-    testerarr = numerator / denominator
-    vector1arr = np.zeros((4000 * all_samples.shape[0]))
-    vector2arr = np.zeros((4000 * all_samples.shape[0]))
-
+    # Generate the indices for the first and second halves of the boxes per sample
+    first_half_indices = np.zeros(2000 * all_samples.shape[0])
+    second_half_indices = np.zeros(2000 * all_samples.shape[0])
     for index in range(all_samples.shape[0]):
-        vector = range(index * 4000, index * 4000 + 2000)
-        vector1arr[index * 4000 : (index + 1) * 4000 - 2000] = vector
+        first_half_indices[index * 2000 : (index + 1) * 2000] = range(
+            index * 4000, index * 4000 + 2000
+        )
+        second_half_indices[index * 2000: (index + 1) * 2000] = range(
+            index * 4000 + 2000, (index + 1) * 4000
+        )
+    first_half_indices = first_half_indices.astype(int)
+    second_half_indices = second_half_indices.astype(int)
 
-    for index in range(all_samples.shape[0]):
-        vector = range(index * 4000 + 2000, index * 4000 + 4000)
-        vector2arr[index * 4000 + 2000 : (index + 1) * 4000] = vector
-
-    vector1arr[0] = 1
-    vector1arr = vector1arr[vector1arr != 0]
-    vector1arr[0] = 0
-    vector2arr = vector2arr[vector2arr != 0]
-    vector1arr = vector1arr.astype(int)
-    vector2arr = vector2arr.astype(int)
-
-    diff = testerarr[vector1arr] - testerarr[vector2arr]
+    # These are the Haar-like features, the difference in average intensity between
+    # paired boxes
+    diff = box_averages[first_half_indices] - box_averages[second_half_indices]
     diff = np.reshape(diff, (all_samples.shape[0], 2000))
-    dist = all_samples - resampled_fid[0:3]
-    prob = np.sqrt(dist[:, 0] ** 2 + dist[:, 1] ** 2 + dist[:, 2] ** 2)
-    prob = np.exp(-0.5 * prob)  # weighted more towards closer distances
+
+    # The position of each sample relative to the fiducial.
+    dist = np.linalg.norm(all_samples - resampled_fid[0:3], axis=1)
+    prob = np.exp(-0.5 * dist)  # weighted more towards closer distances
 
     return [np.hstack((diff[index], prob[index])) for index in range(prob.shape[0])]
 
 
 def train_afid_model(
     afid_num: int,
-    subject_paths: Sequence[PathLike[str] | str],
-    fcsv_paths: Sequence[PathLike[str] | str],
-    feature_offsets_path: PathLike | str,
+    subject_paths: Iterable[PathLike[str] | str],
+    fcsv_paths: Iterable[PathLike[str] | str],
+    feature_offsets: tuple[NDArray, NDArray],
 ):
     """Train a regRF model for a fiducial."""
     finalpred = np.asarray(
         it.chain.from_iterable(
-            gen_features(subject_path, fcsv_path, feature_offsets_path, afid_num)
-            for subject_path, fcsv_path in zip(subject_paths, fcsv_paths, strict=True)
+            gen_features(
+                subject_path, get_fid(fcsv_path, afid_num - 1), feature_offsets
+            )
+            for subject_path, fcsv_path in zip(subject_paths, fcsv_paths)
         )
     )
     print(finalpred.shape)
@@ -235,5 +287,11 @@ def train_all_afid_models(
     feature_offsets_path: PathLike | str,
 ):
     """Train a regRF fiducial for each of the 32 AFIDs."""
+    feature_offsets = np.load(feature_offsets_path)
     for afid_num in range(1, 33):
-        train_afid_model(afid_num, subject_paths, fcsv_paths, feature_offsets_path)
+        train_afid_model(
+            afid_num,
+            subject_paths,
+            fcsv_paths,
+            (feature_offsets["arr_0"], feature_offsets["arr_1"]),
+        )
